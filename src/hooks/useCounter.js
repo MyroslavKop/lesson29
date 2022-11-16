f5367d44e50edd4ee1302a3999ab77ee2f4ac3f8
MiroslavKop <kopytovmiroslav@gmail.com>
2022-11-16 17:22:54 +0200

    lesson29

---
+++ src/hooks/useCounter.js
@@ -0,0 +1,20 @@
+import {useState} from "react";
+
+const useCounter = (initialValue = 1) => {
+    const [count, setCount] = useState(initialValue);
+
+    const increment = () => setCount(prevValue => prevValue + 1);
+    const decrement = () => setCount(prevValue => prevValue - 1);
+    const reset = () => setCount(initialValue);
+
+    return {
+        count,
+        setCount,
+        increment,
+        decrement,
+        reset,
+        initialValue,
+    };
+};
+
+export default useCounter;
